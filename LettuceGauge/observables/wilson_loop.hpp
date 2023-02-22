b8ab04a462cbc98545e2c3d77ac5c5e11cd0c81f
Timo Eichhorn <timo.eichhorn@protonmail.com>
2023-02-22 18:57:33 +0100

    Fixed incorrect implementation of RectangularLoop function template Added function template LineProduct which allows for computing the product of links along a straight line Refactoring of code

--- LettuceGauge/observables/wilson_loop.hpp
+++ LettuceGauge/observables/wilson_loop.hpp
@@ -3,6 +3,7 @@
 
 // Non-standard library headers
 #include <Eigen/Dense>
+#include "../coords.hpp"
 //----------------------------------------
 // Standard library headers
 #include <omp.h>
@@ -19,7 +20,9 @@
 //       For instance, we could left multiply with the adjoint/inverse and right multiply with a new link, which
 //       might be computationally advantageous for larger chain lengths (only 2 multiplications instead of N)
 
-template<int Nmu_start, int Nmu_end, bool reset>
+// TODO: The naming might be misleading, since this function doesn't calculate a single Wilson loop, but rather the expectation value of all loops on a config
+//       In the future, we might want a function to compute single loops...
+template<int N_mu_start, int N_mu_end, bool reset>
 [[nodiscard]]
 double WilsonLoop(const GaugeField& U, GaugeField& U_chain) noexcept
 {
@@ -37,7 +40,7 @@ double WilsonLoop(const GaugeField& U, GaugeField& U_chain) noexcept
                 U_chain({t, x, y, z, mu}).setIdentity();
             }
         }
-        for (int n = Nmu_start; n < Nmu_end; ++n)
+        for (int n = N_mu_start; n < N_mu_end; ++n)
         {
             U_chain({t, x, y, z, 0}) *= U({(t + n)%Nt, x, y, z, 0});
             U_chain({t, x, y, z, 1}) *= U({t, (x + n)%Nx, y, z, 1});
@@ -48,16 +51,16 @@ double WilsonLoop(const GaugeField& U, GaugeField& U_chain) noexcept
     #pragma omp parallel for reduction(+: W)
     for (int t = 0; t < Nt; ++t)
     {
-        int tp {(t + Nmu_end)%Nt};
+        int tp {(t + N_mu_end)%Nt};
         for (int x = 0; x < Nx; ++x)
         {
-            int xp {(x + Nmu_end)%Nx};
+            int xp {(x + N_mu_end)%Nx};
             for (int y = 0; y < Ny; ++y)
             {
-                int yp {(y + Nmu_end)%Ny};
+                int yp {(y + N_mu_end)%Ny};
                 for (int z = 0; z < Nz; ++z)
                 {
-                    int zp {(z + Nmu_end)%Nz};
+                    int zp {(z + N_mu_end)%Nz};
                     // W += (U_chain({t, x, y, z, 0}) * U_chain({tp, x, y, z, 1}) * U_chain({t, xp, y, z, 0}).adjoint() * U_chain({t, x, y, z, 1}).adjoint()).cast<std::complex<double>>();
                     // W += (U_chain({t, x, y, z, 0}) * U_chain({tp, x, y, z, 2}) * U_chain({t, x, yp, z, 0}).adjoint() * U_chain({t, x, y, z, 2}).adjoint()).cast<std::complex<double>>();
                     // W += (U_chain({t, x, y, z, 0}) * U_chain({tp, x, y, z, 3}) * U_chain({t, x, y, zp, 0}).adjoint() * U_chain({t, x, y, z, 3}).adjoint()).cast<std::complex<double>>();
@@ -87,36 +90,58 @@ double WilsonLoop(const GaugeField& U, GaugeField& U_chain) noexcept
 }
 
 
-// TODO: This is still incorrect
+// Computes the product of N_mu links along a straight path in direction mu (a negative N_mu corresponds to moving backwards), starting from current_site
+template<int N_mu>
+[[nodiscard]]
+Matrix_3x3 LineProduct(const GaugeField& U, const site_coord& current_site, const int mu) noexcept
+{
+    static_assert(N_mu != 0, "The template parameter of LineProduct is not allowed to be 0!");
+    // Check sign of N_mu to see if we go forwards or backwards
+    constexpr int          sign_mu  {(N_mu > 0) - (N_mu < 0)};
+    // As of C++20, std::abs is not yet constexpr (coming in C++23), so for now multiply with sign_mu (should be okay since we asserted N_mu != 0)
+    constexpr unsigned int dist_mu  {sign_mu * N_mu};
+    if constexpr(sign_mu > 0)
+    {
+        site_coord tmp_site     {current_site};
+        Matrix_3x3 link_product {U(tmp_site, mu)};
+        // Since we already initialized link_product as U(current_site, mu), the loop starts from 1
+        for (int mu_count = 1; mu_count < dist_mu; ++mu_count)
+        {
+            tmp_site = Move<1>(tmp_site, mu);
+            link_product *= U(tmp_site, mu);
+        }
+        return link_product;
+    }
+    else
+    {
+        site_coord tmp_site     {Move<-1>(current_site, mu)};
+        Matrix_3x3 link_product {U(tmp_site, mu).adjoint()};
+        // Since we already initialized link_product as U(current_site - mu, mu), the loop starts from 1
+        for (int mu_count = 1; mu_count < dist_mu; ++mu_count)
+        {
+            tmp_site = Move<-1>(tmp_site, mu);
+            link_product *= U(tmp_site, mu).adjoint();
+        }
+        return link_product;
+    }
+}
+
 template<int N_mu, int N_nu>
 [[nodiscard]]
-Matrix_3x3 RectangularLoop(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
+Matrix_SU3 RectangularLoop(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
 {
-    Matrix_3x3 tmp {Matrix_3x3::Identity()};
-    for (int mu = 0; mu < N_mu; ++mu)
-    {
-        tmp *= U(Move<1>(current_site, mu), mu);
-    }
-    for (int mu = 0; mu < N_mu; ++mu)
-    {
-        tmp *= U(Move<1>(current_site, nu), nu);
-    }
-    for (int mu = 0; mu < N_mu; ++mu)
-    {
-        tmp *= U(Move<1>(current_site, mu), mu).adjoint();
-    }
-    for (int mu = 0; mu < N_mu; ++mu)
-    {
-        tmp *= U(Move<1>(current_site, mu), mu).adjoint();
-    }
-    return tmp;
+    static_assert(N_mu != 0 and N_nu != 0, "The template parameters of RectangularLoop are not allowed to be 0!");
+    site_coord site_mup     {Move<N_mu>(current_site, mu)};
+    site_coord site_mup_nup {Move<N_nu>(site_mup,     nu)};
+    site_coord site_nup     {Move<N_nu>(current_site, nu)};
+    return LineProduct<N_mu>(U, current_site, mu) * LineProduct<N_nu>(U, site_mup, nu) * LineProduct<-N_mu>(U, site_mup_nup, mu) * LineProduct<-N_nu>(U, site_nup, nu);
 }
 
 // Template specializations for 1x2 and 2x1 loops used in rectangular gauge actions
 
 template<>
 [[nodiscard]]
-Matrix_3x3 RectangularLoop<1, 2>(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
+Matrix_SU3 RectangularLoop<1, 2>(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
 {
     site_coord site_mup     {Move<1>(current_site, mu)};
     site_coord site_mup_nup {Move<1>(site_mup    , nu)};
@@ -127,7 +152,7 @@ Matrix_3x3 RectangularLoop<1, 2>(const GaugeField& U, const site_coord& current_
 
 template<>
 [[nodiscard]]
-Matrix_3x3 RectangularLoop<2, 1>(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
+Matrix_SU3 RectangularLoop<2, 1>(const GaugeField& U, const site_coord& current_site, const int mu, const int nu) noexcept
 {
     site_coord site_mup     {Move<1>(current_site, mu)};
     site_coord site_mupp    {Move<1>(site_mup    , mu)};
