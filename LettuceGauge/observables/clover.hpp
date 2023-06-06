998f206663faa8dac5db4426b5b6433fe2f3b47f
Timo Eichhorn <timo.eichhorn@protonmail.com>
2023-06-06 17:48:26 +0200

    Performance optimization to functions that calculate the clover derivatives based on the antisymmetry of the R terms, which should also slightly improve the performance of the MetaD-HMC

--- LettuceGauge/observables/clover.hpp
+++ LettuceGauge/observables/clover.hpp
@@ -151,7 +151,103 @@ void CalculateClover<1, 1>(const GaugeField& U, FullTensor& Clov) noexcept
     }
 }
 
-// TODO: How do we generalize the function for abritrary clover sizes?
+// // TODO: How do we generalize the function for abritrary clover sizes?
+// [[nodiscard]]
+// Matrix_3x3 CloverDerivativeComponent(const GaugeField& U, const FullTensor& Clover, const site_coord& current_site, const int mu, const int nu, const int rho, const int sigma) noexcept
+// {
+//     site_coord site_mup     = Move< 1>(current_site, mu);
+//     site_coord site_nup     = Move< 1>(current_site, nu);
+//     site_coord site_nud     = Move<-1>(current_site, nu);
+//     site_coord site_mup_nup = Move< 1>(site_mup, nu);
+//     site_coord site_mup_nud = Move<-1>(site_mup, nu);
+
+//     // return U(current_site, mu) * (U(site_mup, nu)               * U(site_nup, mu).adjoint()        * U(current_site, nu).adjoint() * Clover(current_site, rho, sigma)
+//     //                             + U(site_mup, nu)               * U(site_nup, mu).adjoint()        * Clover(site_nup, rho, sigma)  * U(current_site, nu).adjoint()
+//     //                             + U(site_mup, nu)               * Clover(site_mup_nup, rho, sigma) * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+//     //                             + Clover(site_mup, rho, sigma)  * U(site_mup, nu)                  * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+//     //                             - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * U(site_nud, nu)               * Clover(current_site, rho, sigma)
+//     //                             - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * Clover(site_nud, rho, sigma)  * U(site_nud, nu)
+//     //                             - U(site_mup_nud, nu).adjoint() * Clover(site_mup_nud, rho, sigma) * U(site_nud, mu).adjoint()     * U(site_nud, nu)
+//     //                             - Clover(site_mup, rho, sigma)  * U(site_mup_nud, nu).adjoint()    * U(site_nud, mu).adjoint()     * U(site_nud, nu));
+//     // We can move the first multiplication outside into the function CloverDerivative(), would go from 6 -> 1 multiplications (although relatively speaking it's not that much)
+//     // TODO: Factor out more common products? Might slightly improve performance at the cost of readability
+//     return (U(site_mup, nu)               * U(site_nup, mu).adjoint()        * U(current_site, nu).adjoint() * Clover(current_site, rho, sigma)
+//           + U(site_mup, nu)               * U(site_nup, mu).adjoint()        * Clover(site_nup, rho, sigma)  * U(current_site, nu).adjoint()
+//           + U(site_mup, nu)               * Clover(site_mup_nup, rho, sigma) * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+//           + Clover(site_mup, rho, sigma)  * U(site_mup, nu)                  * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+//           - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * U(site_nud, nu)               * Clover(current_site, rho, sigma)
+//           - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * Clover(site_nud, rho, sigma)  * U(site_nud, nu)
+//           - U(site_mup_nud, nu).adjoint() * Clover(site_mup_nud, rho, sigma) * U(site_nud, mu).adjoint()     * U(site_nud, nu)
+//           - Clover(site_mup, rho, sigma)  * U(site_mup_nud, nu).adjoint()    * U(site_nud, mu).adjoint()     * U(site_nud, nu));
+// }
+
+// // TODO: How do we generalize the function for abritrary clover sizes?
+// [[nodiscard]]
+// Matrix_3x3 CloverDerivative(const GaugeField& U, const FullTensor& Clover, const site_coord& current_site, const int mu) noexcept
+// {
+//     Matrix_3x3 derivative_component {Matrix_3x3::Zero()};
+//     // This is basically epsilon_{mu, nu, rho, sigma} manually worked out (not sure if writing a function makes sense?)
+//     switch (mu)
+//     {
+//         case 0:
+//         {
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 2, 3);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 3, 2);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 1, 3);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 3, 1);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 1, 2);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 2, 1);
+//         }
+//         break;
+//         case 1:
+//         {
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 2, 3);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 3, 2);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 0, 3);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 3, 0);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 0, 2);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 2, 0);
+//         }
+//         break;
+//         case 2:
+//         {
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 1, 3);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 3, 1);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 0, 3);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 3, 0);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 0, 1);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 1, 0);
+//         }
+//         break;
+//         case 3:
+//         {
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 1, 2);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 2, 1);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 0, 2);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 2, 0);
+//             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 0, 1);
+//             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 1, 0);
+//         }
+//         break;
+//     }
+//     // The link U(current_site, mu) is a common factor appearing in all 6 components of the clover derivative
+//     derivative_component = U(current_site, mu) * derivative_component;
+//     return -static_cast<floatT>(1.0/(256.0 * pi<floatT> * pi<floatT>)) * SU3::Projection::Algebra(derivative_component);
+//     // // This is still missing the generators in front
+//     // // Also, this is technically only half of the whole derivative term
+//     // // The remaining half however is simply the adjoint
+//     // // Including the adjoint and simplifying some things, we get two times the real part in the end
+//     // // Without the trace we would get two times the hermitian part, with the trace it's only the real part (?)
+//     // // derivative_component = -static_cast<floatT>(4.0/64.0) * (static_cast<floatT>(1.0/3.0) * derivative_component.trace() * Matrix_3x3::Identity() - derivative_component).real();
+//     // Matrix_3x3 tmp {derivative_component - derivative_component.adjoint()};
+//     // // TODO: I think this is not correct yet, since it includes the prefactor coming from the field strength tensor, but not the prefactor 1/(32 pi^2) from the charge definition
+//     // // return -static_cast<floatT>(1.0/32.0) * (tmp - static_cast<floatT>(1.0/3.0) * tmp.trace() * Matrix_3x3::Identity());
+//     // // This should hopefully be correct
+//     // // return -static_cast<floatT>(1.0/(1024.0 * pi<floatT> * pi<floatT>)) * (tmp - static_cast<floatT>(1.0/3.0) * tmp.trace() * Matrix_3x3::Identity());
+//     // // Turns out it wasn't correct... Comparison with numerical derivatives revealed a missing factor 2 (not sure where it comes from yet)
+//     // return -static_cast<floatT>(1.0/(512.0 * pi<floatT> * pi<floatT>)) * (tmp - static_cast<floatT>(1.0/3.0) * tmp.trace() * Matrix_3x3::Identity());
+// }
+
 [[nodiscard]]
 Matrix_3x3 CloverDerivativeComponent(const GaugeField& U, const FullTensor& Clover, const site_coord& current_site, const int mu, const int nu, const int rho, const int sigma) noexcept
 {
@@ -161,24 +257,22 @@ Matrix_3x3 CloverDerivativeComponent(const GaugeField& U, const FullTensor& Clov
     site_coord site_mup_nup = Move< 1>(site_mup, nu);
     site_coord site_mup_nud = Move<-1>(site_mup, nu);
 
-    // return U(current_site, mu) * (U(site_mup, nu)               * U(site_nup, mu).adjoint()        * U(current_site, nu).adjoint() * Clover(current_site, rho, sigma)
-    //                             + U(site_mup, nu)               * U(site_nup, mu).adjoint()        * Clover(site_nup, rho, sigma)  * U(current_site, nu).adjoint()
-    //                             + U(site_mup, nu)               * Clover(site_mup_nup, rho, sigma) * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
-    //                             + Clover(site_mup, rho, sigma)  * U(site_mup, nu)                  * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
-    //                             - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * U(site_nud, nu)               * Clover(current_site, rho, sigma)
-    //                             - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * Clover(site_nud, rho, sigma)  * U(site_nud, nu)
-    //                             - U(site_mup_nud, nu).adjoint() * Clover(site_mup_nud, rho, sigma) * U(site_nud, mu).adjoint()     * U(site_nud, nu)
-    //                             - Clover(site_mup, rho, sigma)  * U(site_mup_nud, nu).adjoint()    * U(site_nud, mu).adjoint()     * U(site_nud, nu));
     // We can move the first multiplication outside into the function CloverDerivative(), would go from 6 -> 1 multiplications (although relatively speaking it's not that much)
-    // TODO: Factor out more common products? Might slightly improve performance at the cost of readability
-    return (U(site_mup, nu)               * U(site_nup, mu).adjoint()        * U(current_site, nu).adjoint() * Clover(current_site, rho, sigma)
-          + U(site_mup, nu)               * U(site_nup, mu).adjoint()        * Clover(site_nup, rho, sigma)  * U(current_site, nu).adjoint()
-          + U(site_mup, nu)               * Clover(site_mup_nup, rho, sigma) * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
-          + Clover(site_mup, rho, sigma)  * U(site_mup, nu)                  * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
-          - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * U(site_nud, nu)               * Clover(current_site, rho, sigma)
-          - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()        * Clover(site_nud, rho, sigma)  * U(site_nud, nu)
-          - U(site_mup_nud, nu).adjoint() * Clover(site_mup_nud, rho, sigma) * U(site_nud, mu).adjoint()     * U(site_nud, nu)
-          - Clover(site_mup, rho, sigma)  * U(site_mup_nud, nu).adjoint()    * U(site_nud, mu).adjoint()     * U(site_nud, nu));
+    // We can also replace all clover terms C_{rho, sigma} with R_{rho, sigma} = C_{rho, sigma} - C_{sigma, rho}, which means we only need half of the clover terms derivative components (due to linearity and antisymmetry of R)
+    Matrix_3x3 R_current_site {Clover(current_site, rho, sigma) - Clover(current_site, sigma, rho)};
+    Matrix_3x3 R_site_nup     {Clover(    site_nup, rho, sigma) - Clover(    site_nup, sigma, rho)};
+    Matrix_3x3 R_site_mup_nup {Clover(site_mup_nup, rho, sigma) - Clover(site_mup_nup, sigma, rho)};
+    Matrix_3x3 R_site_mup     {Clover(    site_mup, rho, sigma) - Clover(    site_mup, sigma, rho)};
+    Matrix_3x3 R_site_nud     {Clover(    site_nud, rho, sigma) - Clover(    site_nud, sigma, rho)};
+    Matrix_3x3 R_site_mup_nud {Clover(site_mup_nud, rho, sigma) - Clover(site_mup_nud, sigma, rho)};
+    return (U(site_mup, nu)               * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint() * R_current_site
+          + U(site_mup, nu)               * U(site_nup, mu).adjoint()     * R_site_nup                    * U(current_site, nu).adjoint()
+          + U(site_mup, nu)               * R_site_mup_nup                * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+          + R_site_mup                    * U(site_mup, nu)               * U(site_nup, mu).adjoint()     * U(current_site, nu).adjoint()
+          - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()     * U(site_nud, nu)               * R_current_site
+          - U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()     * R_site_nud                    * U(site_nud, nu)
+          - U(site_mup_nud, nu).adjoint() * R_site_mup_nud                * U(site_nud, mu).adjoint()     * U(site_nud, nu)
+          - R_site_mup                    * U(site_mup_nud, nu).adjoint() * U(site_nud, mu).adjoint()     * U(site_nud, nu));
 }
 
 // TODO: How do we generalize the function for abritrary clover sizes?
@@ -189,44 +283,33 @@ Matrix_3x3 CloverDerivative(const GaugeField& U, const FullTensor& Clover, const
     // This is basically epsilon_{mu, nu, rho, sigma} manually worked out (not sure if writing a function makes sense?)
     switch (mu)
     {
+        // Only three terms per direction are needed here since we can use the antisymmetry of R_{rho, sigma} = C_{rho, sigma} - C_{sigma, rho}
         case 0:
         {
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 2, 3);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 3, 2);
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 1, 3);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 3, 1);
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 1, 2);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 2, 1);
         }
         break;
         case 1:
         {
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 2, 3);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 3, 2);
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 0, 3);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 3, 0);
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 0, 2);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 2, 0);
         }
         break;
         case 2:
         {
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 1, 3);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 3, 1);
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 0, 3);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 3, 0);
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 3, 0, 1);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 3, 1, 0);
         }
         break;
         case 3:
         {
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 0, 1, 2);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 0, 2, 1);
             derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 1, 0, 2);
-            derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 1, 2, 0);
             derivative_component -= CloverDerivativeComponent(U, Clover, current_site, mu, 2, 0, 1);
-            derivative_component += CloverDerivativeComponent(U, Clover, current_site, mu, 2, 1, 0);
         }
         break;
     }
