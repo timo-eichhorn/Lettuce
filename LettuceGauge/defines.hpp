6d65600ebee2fecc1cd7cd06a718a43302fcae3f
timo_eichhorn <timo.eichhorn@protonmail.com>
2023-06-08 02:58:37 +0200

    Removed unused code from defines.hpp

--- LettuceGauge/defines.hpp
+++ LettuceGauge/defines.hpp
@@ -2,7 +2,6 @@
 #define LETTUCE_DEFINES_HPP
 
 // Non-standard library headers
-// #include "math/random/pcg/pcg_random.hpp"
 #include "../PCG/pcg_random.hpp"
 #include <unsupported/Eigen/MatrixFunctions>
 #include <Eigen/Dense>
@@ -40,34 +39,26 @@ template<typename T>
 inline constexpr T pi {static_cast<T>(3.14159265358979323846L)};
 
 using floatT = double;
-// TODO: Set default epsilon for iterative stuff?
-// if constexpr(std::is_same(floatT, float)::value)
-// {
-
-// }
 
 //-----
 
 int n_run;                                                  // Number of runs
 double n_run_inverse;                                       // Inverse number of runs
 int expectation_period;                                     // Number of updates between calculation of expectation values
-// inline constexpr int n_smear {20};                           // Number of smearing steps (total amount of smearing steps is actually 1 + (n_smear - 1) * n_smear_skip)
-// inline constexpr int n_smear_skip {10};                      // Number of smearing steps to skip between measurements
-// inline constexpr floatT rho_stout {0.04};                   // Stout smearing parameter
-inline int n_smear {7};                                   // Number of smearing steps (total amount of smearing steps is actually 1 + (n_smear - 1) * n_smear_skip)
+inline int n_smear {7};                                     // Number of smearing steps (total amount of smearing steps is actually n_smear * n_smear_skip)
 inline int n_smear_skip {5};                                // Number of smearing steps to skip between measurements
 inline floatT rho_stout {0.12};                             // Stout smearing parameter
 inline constexpr int n_metro {0};                           // Number of Metropolis sweeps per total update sweep
 inline constexpr int multi_hit {8};                         // Number of hits per site in Metropolis algorithm
-inline constexpr int n_heatbath {0};                        // Number of heat bath sweeps per total update sweep
-inline constexpr int n_hmc {5};                             // Number of integration steps per HMC update
-inline constexpr int n_orelax {0};                          // Number of overrelaxation sweeps per total update sweep
+inline constexpr int n_heatbath {1};                        // Number of heat bath sweeps per total update sweep
+inline constexpr int n_hmc {0};                             // Number of integration steps per HMC update
+inline constexpr int n_orelax {4};                          // Number of overrelaxation sweeps per total update sweep
 inline constexpr int n_instanton_update {0};                // Number of instanton updates per total update sweep
-inline constexpr bool metadynamics_enabled {true};          // Enable metadynamics updates or not
-inline constexpr bool metapotential_updated {true};         // If true, update the metapotential with every update, if false, simulate with a static metapotential
+inline constexpr bool metadynamics_enabled {false};          // Enable metadynamics updates or not
+inline constexpr bool metapotential_updated {false};         // If true, update the metapotential with every update, if false, simulate with a static metapotential
 inline constexpr int n_smear_meta {4};                      // Number of smearing steps for topological charge used in Metadynamics
 inline constexpr bool tempering_enabled {false};             // Enable metadynamics updates with tempering or not
-inline constexpr int tempering_nonmetadynamics_sweeps {1};  // Number of non metadynamics update sweeps for every metadynamics update during tempering
+inline constexpr int tempering_nonmetadynamics_sweeps {10};  // Number of non metadynamics update sweeps for every metadynamics update during tempering
 inline constexpr int tempering_swap_period {1};             // Number of update sweeps between parallel tempering swap attempts
 inline double metro_norm {1.0};                             // Norm for Metropolis update. CAUTION: Needs to be set to correct value inside Configuration() function
 inline double metro_target_acceptance {0.5};                // Target acceptance rate for Metropolis update, values around 50% seem to work well, but TRY OUT!
@@ -84,7 +75,6 @@ auto start {std::chrono::system_clock::now()};              // Start time
 floatT beta;                                                // Coupling
 std::ofstream datalog;                                      // Output stream to save data
 std::ofstream wilsonlog;                                    // Output stream to save data (Wilson loops)
-//std::random_device randomdevice;                          // Creates random device to seed PRNGs
 pcg_extras::seed_seq_from<std::random_device> seed_source;  // Seed source to seed PRNGs
 #ifdef FIXED_SEED                                           // PRNG for random coordinates and probabilites
 pcg64 generator_rand(1);
@@ -105,22 +95,6 @@ uint_fast64_t acceptance_count_instanton         {0};       // Instanton update 
 using std::array;
 using std::vector;
 
-// template<typename T>
-// using 3x3mat = Eigen::Matrix<std::complex<T>, 3, 3>;
-
-// using 3x3mat_f = Eigen::Matrix<std::complex<floatT>, 3, 3>;
-// using 3x3mat_d = Eigen::Matrix<std::complex<double>, 3, 3>;
-
-// template<typename T, size_t Nz, size_t Ny, size_t Nx, size_t Nt>
-// using Gl_Lattice = array<array<array<array<array<3x3mat, 4>, Nz>, Ny>, Nx>, Nt>;
-
-// template<typename T, size_t Nz, size_t Ny, size_t Nx, size_t Nt>
-// using Spatial_tensor = array<array<array<array<array<array<3x3mat, 3>, 3>, Nz>, Ny>, Nx>, Nt>;
-
-// template<typename T, size_t Nz, size_t Ny, size_t Nx, size_t Nt>
-// using Full_tensor = array<array<array<array<array<array<3x3mat, 4>, 4>, Nz>, Ny>, Nx>, Nt>;
-
-// typedef Eigen::Matrix<std::complex<double>, 3, 3> Matrix_SU3_double;
 // NxN_matrix is the same type as SUN_matrix, the different names are only meant to distinguish between
 // SU(N) group elements and NxN matrices mathematically
 using Matrix_2x2     = Eigen::Matrix<std::complex<floatT>, 2, 2>;
@@ -128,9 +102,6 @@ using Matrix_SU2     = Matrix_2x2;
 using Matrix_3x3     = Eigen::Matrix<std::complex<floatT>, 3, 3>;
 using Matrix_SU3     = Matrix_3x3;
 using Local_tensor   = array<array<Matrix_SU3, 4>, 4>;
-// using Gl_Lattice     = array<array<array<array<array<Matrix_SU3, 4>, Nz>, Ny>, Nx>, Nt>;
-// using Spatial_tensor = array<array<array<array<array<array<Matrix_SU3, 3>, 3>, Nz>, Ny>, Nx>, Nt>;
-// using Full_tensor    = array<array<array<array<array<array<Matrix_SU3, 4>, 4>, Nz>, Ny>, Nx>, Nt>;
 
 //-----
 // Better complex numbers?
@@ -165,7 +136,6 @@ template<typename T>
 [[nodiscard]]
 constexpr int sign(T x)
 {
-    // return (x > T(0)) - (x < T(0));
     return (x > static_cast<T>(0)) - (x < static_cast<T>(0));
 }
 
