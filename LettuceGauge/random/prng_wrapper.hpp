2a780acc96d896dcd1939d08e390563d18fe45e9
timo_eichhorn <timo.eichhorn@protonmail.com>
2023-06-12 02:52:55 +0200

    Properly implemented functions to read and write PRNG and normal distribution states to and from files Bug fixes and refactoring of code in prng_wrapper.hpp

--- LettuceGauge/random/prng_wrapper.hpp
+++ LettuceGauge/random/prng_wrapper.hpp
@@ -2,12 +2,13 @@
 #define LETTUCE_PRNG_WRAPPER_HPP
 
 // Non-standard library headers
-// ...
+#include "../IO/ansi_colors.hpp"
 //----------------------------------------
 // Standard library headers
 // ...
 //----------------------------------------
 // Standard C++ headers
+#include <filesystem>
 #include <fstream>
 #include <random>
 #include <string>
@@ -28,7 +29,8 @@
 //+---------------------------------------------------------------------------------+
 
 // Per default, use floatT set in defines.hpp as floating point type and standard ints as integer type
-template<typename prngT, typename floatT = floatT, typename intT = int>
+// template<typename prngT, typename floatT = floatT, typename intT = int>
+template<typename prngT, typename floatT, typename intT = int>
 class PRNG
 {
     private:
@@ -42,29 +44,14 @@ class PRNG
         std::vector<std::uniform_real_distribution<floatT>> uniform_real_distributions;
         std::vector<std::uniform_int_distribution<intT>>    uniform_int_distributions;
         std::size_t                                         n_prng;
-        using prng_type  = prngT;
-        // TODO: Alias for state type and size of prngT like in Grid?
-        using float_type = floatT;
-        using int_type   = intT;
+        using prng_type       = prngT;
+        // TODO: Alias for state type and size of prngT like in Grid? Problematic, since not all PRNGs seem to provide a way to check the state size?
+        using float_type      = floatT;
+        using int_type        = intT;
 
         template<typename seed_sourceT>
         explicit PRNG(const std::size_t n_prng_in, seed_sourceT&& seed_source) noexcept :
-        // random_generators(n_prng_in, seed_source), normal_distributions(n_prng_in, {0.0, 1.0}), uniform_real_distributions(n_prng_in, {0.0, 1.0}), uniform_int_distributions(n_prng_in, {1, 8}), n_prng(n_prng_in)
         random_generators(n_prng_in), normal_distributions(n_prng_in, std::normal_distribution<floatT>(0.0, 1.0)), uniform_real_distributions(n_prng_in, std::uniform_real_distribution<floatT>(0.0, 1.0)), uniform_int_distributions(n_prng_in, std::uniform_int_distribution<intT>(1, 8)), n_prng(n_prng_in)
-        {
-            #ifdef FIXED_SEED
-            // Ignore seed_source and seed all PRNGs with 1
-            SeedPRNGs(1);
-            #else
-            // Seed all PRNGs with the provided seed_source
-            SeedPRNGs(std::forward<seed_sourceT>(seed_source));
-            #endif
-        }
-
-        // Constructor that takes a lattice as argument to create a PRNG object of matching size
-        template<typename seed_sourceT>
-        PRNG(const GaugeField& U, seed_sourceT&& seed_source) noexcept :
-        random_generators(U.Volume()), normal_distributions(U.Volume(), std::normal_distribution<floatT>(0.0, 1.0)), uniform_real_distributions(U.Volume(), std::uniform_real_distribution<floatT>(0.0, 1.0)), uniform_int_distributions(U.Volume(), std::uniform_int_distribution<intT>(1, 8)), n_prng(U.Volume())
         {
             #ifdef FIXED_SEED
             // Ignore seed_source and seed all PRNGs with 1
@@ -93,43 +80,112 @@ class PRNG
             random_generators[prng_index].seed(seed_source);
         }
 
-        // TODO: Need to save the distribution states as well (at least for the normal distribution)
-        void SaveState(const std::string& filename, const bool overwrite = false)
+        // TODO: Is there a portable way to write the states in binary format?
+        // TODO: When using CUDA, we can't write to files from device functions, so (only?) provide functions that save/load state to/from vector?
+        // lttc_host
+        void SavePRNGState(const std::string& filename)
         {
-            std::ofstream state_stream_out(filename, std::ios::binary);
+            std::ofstream state_stream(filename);
             for (auto& prng : random_generators)
             {
-                state_stream_out << prng;
-                // TODO: Some kind of delimiter??
+                state_stream << prng << " ";
+            }
+            state_stream.close();
+            //-----
+            if (!state_stream)
+            {
+                std::cerr << Lettuce::Color::BoldRed << "Writing PRNG state to file " << filename << "failed!" << Lettuce::Color::Reset << std::endl;
             }
         }
 
-        // TODO: Need to save the distribution states as well (at least for the normal distribution)
-        void LoadState(const std::string& filename)
+        // lttc_host
+        void LoadPRNGState(const std::string& filename)
         {
-            std::ifstream state_stream_in(filename, std::ios::binary);
-            // TODO: Check size of state in file and check for compatibility
-            for (auto& prng : random_generators)
+            if (!std::filesystem::exists(filename))
             {
-                state_stream_in >> prng;
-                // TODO: Some kind of delimiter??
+                std::cerr << Lettuce::Color::BoldRed << "Error while trying to load PRNG state: File " << filename << "not found!" << Lettuce::Color::Reset << std::endl;
+                return;
             }
+            //-----
+            std::ifstream state_stream(filename);
+            for(auto& prng : random_generators)
+            {
+                state_stream >> prng;
+            }
+            //-----
+            if (!state_stream)
+            {
+                std::cerr << Lettuce::Color::BoldRed << "Reading PRNG state from file " << filename << "failed!" << Lettuce::Color::Reset << std::endl;
+            }
+            state_stream.close();
+        }
+
+        // lttc_host
+        void SaveDistributionState(const std::string& filename)
+        {
+            std::ofstream state_stream(filename);
+            for (auto& normal_distribution : normal_distributions)
+            {
+                state_stream << normal_distribution << " ";
+            }
+            state_stream.close();
+            //-----
+            if (!state_stream)
+            {
+                std::cerr << Lettuce::Color::BoldRed << "Writing PRNG state to file " << filename << "failed!" << Lettuce::Color::Reset << std::endl;
+            }
+        }
+
+        // lttc_host
+        void LoadDistributionState(const std::string& filename)
+        {
+            if (!std::filesystem::exists(filename))
+            {
+                std::cerr << Lettuce::Color::BoldRed << "Error while trying to load normal distribution state: File " << filename << "not found!" << Lettuce::Color::Reset << std::endl;
+                return;
+            }
+            //-----
+            std::ifstream state_stream(filename);
+            for(auto& normal_distribution : normal_distributions)
+            {
+                state_stream >> normal_distribution;
+            }
+            //-----
+            if (!state_stream)
+            {
+                std::cerr << Lettuce::Color::BoldRed << "Reading normal distribution state from file " << filename << "failed!" << Lettuce::Color::Reset << std::endl;
+            }
+            state_stream.close();
+        }
+
+        // lttc_host
+        void LoadState(const std::string& filename_prng, const std::string& filename_normal_distribution)
+        {
+            LoadPRNGState(filename_prng);
+            LoadDistributionState(filename_normal_distribution);
+        }
+
+        // lttc_host
+        void SaveState(const std::string& filename_prng, const std::string& filename_normal_distribution)
+        {
+            SavePRNGState(filename_prng);
+            SaveDistributionState(filename_normal_distribution);
         }
 
         [[nodiscard]]
-        floatT UniformReal(int index) noexcept
+        floatT UniformReal(const std::size_t index) noexcept
         {
             return uniform_real_distributions[index](random_generators[index]);
         }
 
         [[nodiscard]]
-        floatT Gaussian(int index) noexcept
+        floatT Gaussian(const std::size_t index) noexcept
         {
             return normal_distributions[index](random_generators[index]);
         }
 
         [[nodiscard]]
-        intT UniformInt(int index) noexcept
+        intT UniformInt(const std::size_t index) noexcept
         {
             return uniform_int_distributions[index](random_generators[index]);
         }
