862f23ef96d6a7db07c2b76aaf7c6d68e9e30cc2
Timo Eichhorn <timo.eichhorn@protonmail.com>
2023-06-18 00:56:40 +0200

    Changed momentum convention in smd_gauge.hpp to match other molecular-dynamics-based algorithms (momenta are now algebra elements/antihermitian traceless matrices)

--- LettuceGauge/updates/smd_gauge.hpp
+++ LettuceGauge/updates/smd_gauge.hpp
@@ -19,7 +19,6 @@
 // TODO: Implement!
 // In contrast to the HMC, you need to make sure that the Momentum field is not reused/overwritten by other functions,
 // since the momenta are only partially refreshed at the start of a new trajectory!
-// TODO: Change momentum convention to match HMC and MetaD-HMC
 namespace GaugeUpdates
 {
     template<typename IntegratorT, typename ActionT, typename prngT>
@@ -66,15 +65,7 @@ namespace GaugeUpdates
                     floatT     phi7         {global_prng.Gaussian(current_link)};
                     floatT     phi8         {global_prng.Gaussian(current_link)};
 
-                    // TODO: This is the old version/convention where the momenta are not algebra elements, but rather traceless hermitian matrices
-                    // Random momentum in su(3) given by phi_i * T^i (where T^i is 0.5 * i-th Gell-Mann matrix)
-                    // Technically A is a traceless hermitian matrix, while su(3) matrices are anti-hermitian
-                    // Matrix_3x3 A;
-                    // A << std::complex<floatT>(phi3 + phi8/sqrt(3.0),0.0),std::complex<floatT>(phi1,-phi2),std::complex<floatT>(phi4,-phi5),
-                    //      std::complex<floatT>(phi1,phi2),                std::complex<floatT>(-phi3 + phi8/sqrt(3.0),0.0),std::complex<floatT>(phi6,-phi7),
-                    //      std::complex<floatT>(phi4,phi5),                std::complex<floatT>(phi6,phi7),std::complex<floatT>(-2.0*phi8/sqrt(3.0),0.0);
-                    // Momentum({t, x, y, z, mu}) = static_cast<floatT>(0.5) * A;
-                    // TODO: This is the new version where the momenta are algebra elements
+                    // Random momentum in su(3) given by i * phi_i * T^i (where T^i is 0.5 * i-th Gell-Mann/traceless hermitian matrix)
                     Matrix_3x3 A;
                     A << std::complex<floatT>(0.0,phi3 + phi8/sqrt(3.0)), std::complex<floatT>(phi2,phi1),                  std::complex<floatT>(phi5,phi4),
                          std::complex<floatT>(-phi2,phi1),                std::complex<floatT>(0.0,-phi3 + phi8/sqrt(3.0)), std::complex<floatT>(phi7,phi6),
@@ -115,7 +106,7 @@ namespace GaugeUpdates
                 {
                     link_coord current_link {t, x, y, z, mu};
                     Matrix_3x3 tmp {Action.Staple(U, current_link) * U(current_link).adjoint()};
-                    Momentum(current_link) -= epsilon * i<floatT> * beta / 6.0 * SU3::Projection::Algebra(tmp);
+                    Momentum(current_link) += epsilon * beta / 6.0 * SU3::Projection::Algebra(tmp);
                 }
                 // std::cout << "Momenta lie in algebra: " << SU3::Tests::Testsu3All(Momentum, 1e-12) << std::endl;
             }
@@ -133,7 +124,8 @@ namespace GaugeUpdates
                 for (int mu = 0; mu < 4; ++mu)
                 {
                     // Matrix_3x3 tmp_mat {(i<floatT> * epsilon * Momentum({t, x, y, z, mu})).exp()};
-                    Matrix_3x3 tmp_mat {SU3::exp(epsilon * Momentum({t, x, y, z, mu}))};
+                    // Multiplication with -i since SU3::exp() expects a traceless hermitian 3x3 matrix Mat and returns exp(i * Mat)
+                    Matrix_3x3 tmp_mat {SU3::exp(-i<floatT> * epsilon * Momentum({t, x, y, z, mu}))};
                     U({t, x, y, z, mu}) = tmp_mat * U({t, x, y, z, mu});
                     SU3::Projection::GramSchmidt(U({t, x, y, z, mu}));
                 }
@@ -145,10 +137,8 @@ namespace GaugeUpdates
             [[nodiscard]]
             double Hamiltonian() const noexcept
             {
-                // double potential_energy {WilsonAction::Action(U)};
                 double potential_energy {Action.Action(U)};
                 double kinetic_energy   {0.0};
-                // TODO: Momentum * Momentum.adjoint() or Momentum^2? Also is there a prefactor 0.5 or not?
                 #pragma omp parallel for reduction(+: kinetic_energy)
                 for (int t = 0; t < Nt; ++t)
                 for (int x = 0; x < Nx; ++x)
@@ -156,7 +146,7 @@ namespace GaugeUpdates
                 for (int z = 0; z < Nz; ++z)
                 for (int mu = 0; mu < 4; ++mu)
                 {
-                    kinetic_energy += std::real((Momentum({t, x, y, z, mu}) * Momentum({t, x, y, z, mu})).trace());
+                    kinetic_energy -= std::real((Momentum({t, x, y, z, mu}) * Momentum({t, x, y, z, mu})).trace());
                 }
                 // std::cout << "kinetic_energy: " << kinetic_energy << " potential_energy: " << potential_energy << std::endl;
                 return potential_energy + kinetic_energy;
